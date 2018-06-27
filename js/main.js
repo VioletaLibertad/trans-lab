902b3a49fb8254a0f8205f2fd1e5d0fdc4036683
Violeta <morada.libertad@gmail.com>
2018-06-27 17:03:11 -0400

    Avance en JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-/* Holiii acá va tu código también */
+function userValidation() {
+  let userPass = document.getElementById('inputPassword').value;
+  const loginPage = document.getElementsByClassName('loginPage');
+  for (let j = 0; j < userPass.length; j++) {
+    if (userPass.length <= 8) {
+      console.log('very good');
+    } else {
+      console.log('very bad');
+    }
+    loginPage.style.display = 'none';
+  };
+};
