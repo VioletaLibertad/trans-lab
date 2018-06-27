9183a9a953953857314c965d58add0f0e113b63a
Violeta <morada.libertad@gmail.com>
2018-06-27 18:29:18 -0400

    Aparecer y desaparecer funcionando

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 function userValidation() {
+  let userEmail = document.getElementById('inputEmail').value;
   let userPass = document.getElementById('inputPassword').value;
-  const loginPage = document.getElementsByClassName('loginPage');
-  for (let j = 0; j < userPass.length; j++) {
-    if (userPass.length <= 8) {
-      console.log('very good');
-    } else {
-      console.log('very bad');
-    }
-    loginPage.style.display = 'none';
-  };
+  for (let i = 0; i < userEmail.length; i++) {
+    for (let j = 0; j < userPass.length; j++) {
+      if (userEmail.length >= 5 && userEmail[i] === '@') {
+        if (userPass.length <= 8 ) {
+          console.log('very good');
+          const btnLogin = document.getElementById('btn-login');
+          btnLogin.addEventListener('click', () => {
+            document.getElementById('loginPage').classList.add('disappear');
+            document.getElementById('home').classList.remove('disappear');            
+            document.getElementById('home').classList.add('show');
+          });
+        } else {
+          console.log('very bad');
+          return alert('Tu Email o Contraseña no son válidos, inténtalo de nuevo');
+        };
+      }
+    };
+  }
 };
